4d71b89fcea9028512e491e606e73ca62d1e1bbc
Ilya Raskin <iraskin@mail.ru>
2018-09-18 01:15:23 +0300

    task 2

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-const int NUM_OF_VERCE = 33; //Количество строф
+const int NUM_OF_VERCE = 33; //Number of verces
 
-const int VERCE_SIZE = 14; //Размер строфы. Сейчас стоит как в Онегине.
+const int VERCE_SIZE = 14; //Size of verce. Now it is like in Onegin
 
-//Настройки языка
+//language setting
 const int NUM_OF_VEWELS = 12;
 const char* TEXT = "test.txt";
 const char* SORT_TEXT = "sort.txt";
@@ -22,25 +22,25 @@ char vowels_en[12] = {'A', 'E', 'I', 'O', 'U', 'Y', 'a', 'e', 'i', 'o', 'u', 'y'
 char vowels_ru[20] = {'А', 'Е', 'Ё', 'И', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я',
                       'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'};
 
-//Функция заполняет список строк. Возвращает 0 в случае успеха.
-//Также функция заменяет переносы строки в тексте на '\0'. Параметры:
-//text - обрабатываемый текст (массив строк)
-//lines - пустой список, который заполняет функция
-//num_of_lines - количество строк в тексте, заполняется функцией.
+// The function fills the list of strings. It returns 0 if it is successful.
+// Also the function replaces the line breaks in the text with '\0'. Options:
+// text - processed text (array of strings)
+// lines - the empty list that the function fills in
+// num_of_lines - the number of lines in the text, is filled with the function.
 int makeListOfLines(char** text, char*** lines, int* num_of_lines)
 {
     if (*text == nullptr || *lines != nullptr) {
         printf("makeListOfLines: Incorrect input data\n");
         return 1;
     }
-    //Подсчитываем количество строк в тексте
+    // Count the number of lines in the text
     *num_of_lines = 1;
     for (int i = 0; (*text)[i] != '\0'; ++i) {
         if ((*text)[i] == '\n') {
             ++(*num_of_lines);
         }
     }
-    //Заполняем список строк
+    // Fill the list of strings
     *lines = new char*[*num_of_lines + 1];
     (*lines)[0] = *text;
     int j = 1;
@@ -59,30 +59,30 @@ int makeListOfLines(char** text, char*** lines, int* num_of_lines)
     return 0;
 }
 
-//Функция читает текст из файла и выполняет его первичную обработку. Возвращает 0 в случае успеха. Параметры:
-//text - обрабатываемый текст (массив строк). Передаётся в функцию пустым и заполняется ей.
-//lines - пустой список, который заполняет функция (вызовом makeListOfLines).
-//num_of_lines - количество строк в тексте, заполняется функцией (вызовом makeListOfLines).
+// The function reads the text from the file and performs its initial processing. It returns 0 if it is successful. Options:
+// text - processed text (array of strings). It is passed to the function empty and is filled in to it.
+// lines is the empty list that the function fills (by calling makeListOfLines).
+// num_of_lines - the number of lines in the text, is filled with a function (by calling makeListOfLines).
 int readText(char** text, char*** lines, int* num_of_lines)
 {
     if (*text != nullptr || *lines != nullptr) {
         printf("readText: Incorrect input data\n");
         return 1;
     }
-    //Открытие файла
+    // Open the file
     int fd;
     if ((fd = open(TEXT, O_RDONLY)) == 0) {
         printf("readText: Cannot open file.\n");
         return 1;
     }
     struct stat buff;
-    //Заполнение структуры buff
+    // Filling the buff structure
     fstat(fd, &buff);
     *text = new char[buff.st_size + 1];
     (*text)[buff.st_size] = '\0';
-    //Чтение текста
+    // Read the text
     read(fd, *text, buff.st_size);
-    //Подготовка текста к дальнейшему использованию
+    // Preparing the text for further use
     if (makeListOfLines(text, lines, num_of_lines)) {
         printf("readText: Error in makeListOfLines\n");
         return 1;
@@ -91,8 +91,8 @@ int readText(char** text, char*** lines, int* num_of_lines)
     return 0;
 }
 
-//Функция сравнивает 2 строки. Строки сравниваются лексикографически.
-//Возвращает true, если a < b, false иначе.
+// The function compares 2 lines. The lines are compared lexicographically.
+// Returns true if a < b, false otherwise.
 bool comparatorOfStrings(char* a, char* b)
 {
     int i = 0, j = 0;
@@ -120,8 +120,8 @@ bool comparatorOfStrings(char* a, char* b)
     }
 }
 
-//Функция сравнивает 2 строки.Строки сравниваются лексикографически, но начиная с конца.
-//Возвращает true, если a < b, false иначе.
+// The function compares 2 lines. The strings are compared lexicographically, but starting from the end.
+// Returns true if a < b, false otherwise.
 bool reversComparatorOfStrings(char* a, char* b)
 {
     int i = strlen(a) - 1, j = strlen(b) - 1;
@@ -149,19 +149,19 @@ bool reversComparatorOfStrings(char* a, char* b)
     }
 }
 
-//Сортирует строки в lines. Строки сравниваются лексикографически.
+// Sorts the lines in lines. The lines are compared lexicographically.
 void sortStrings(char*** lines, int num_of_lines)
 {
     sort((*lines), (*lines) + num_of_lines, comparatorOfStrings);
 }
 
-//Сортирует строки в lines. Строки сравниваются лексикографически, но с конца.
+// Sorts the lines in lines. The lines are compared lexicographically, but from the end.
 void reversSortStrings(char*** lines, int num_of_lines)
 {
     sort((*lines), (*lines) + num_of_lines, reversComparatorOfStrings);
 }
 
-//Проверяет, является ли символ гласной буквой.
+// Checks if the simbol is a vowel.
 bool isVowel(char a)
 {
     for (int i = 0; i < NUM_OF_VEWELS; ++i) {
@@ -172,7 +172,7 @@ bool isVowel(char a)
     return false;
 }
 
-//Проверят, рифмуются ли 2 строки. Для этого проверяется, совпадают ли их окончания.
+// Check if 2 lines rhyme. For this, it is checked whether their endings coincide.
 bool isLinesRhyme(char* line_1, char* line_2)
 {
     int i = strlen(line_1) - 1, j = strlen(line_2) - 1;
@@ -192,18 +192,18 @@ bool isLinesRhyme(char* line_1, char* line_2)
     return true;
 }
 
-//Функция находит 2 рифмующиеся строки. Параметры:
-//lines - список строк текста.
-//num_of_lines - количество строк (длина lines).
-//rhymes - массив, где для каждой строки лежит указатель на начало диапазона рифмующихся с ней строк. Запоняется лениво.
-//res_1, res_2 - сюда заносятся номера найденных рифмующихся строк.
-//is_used - содержит информацию, какие строки уже использованы.
+// The function finds 2 rhyming strings. Options:
+// lines - a list of lines of text.
+// num_of_lines - number of lines (length of lines).
+// rhymes is an array where for each line there is a pointer to the beginning of the range of rhyming lines with it. It is filled lazy.
+// res_1, res_2 - the numbers of the found rhyming lines are entered here.
+// is_used - contains information about which lines are already used.
 void makeRhyme(char*** lines, int num_of_lines, char**** rhymes, int* res_1, int* res_2, bool** is_used)
 {
-    //Ищем не использованную строку.
+    // Look for an unused string.
     for (*res_1 = rand() % num_of_lines; (*is_used)[*res_1]; (*res_1) = ((*res_1) + 1) % num_of_lines) {}
     (*is_used)[*res_1] = true;
-    int first_rhyme_line = *res_1;//Первая строка, рифмующаяся с данной.
+    int first_rhyme_line = *res_1;// First line that is rhyming with this one.
     while (first_rhyme_line != 0 && (*rhymes)[first_rhyme_line] == nullptr &&
            isLinesRhyme((*lines)[first_rhyme_line], (*lines)[first_rhyme_line - 1])) {
         --first_rhyme_line;
@@ -225,12 +225,12 @@ void makeRhyme(char*** lines, int num_of_lines, char**** rhymes, int* res_1, int
     (*is_used)[*res_2] = true;
 }
 
-//Генерирует стихотворение, случайно переставляя рифмующиеся строки. Параметры:
-//lines - список строк текста.
-//num_of_lines - количество строк (длина lines).
-//generate_text - массив для записи генерируемого стихотворения. Заполняется функцией.
-//rhymes - массив, где для каждой строки лежит указатель на начало диапазона рифмующихся с ней строк. Запоняется лениво.
-//num_generate_lines - размер генерируемого текста.
+// Generates a poem by accidentally rearranging rhyming lines. Options:
+// lines - a list of lines of text.
+// num_of_lines - number of lines (length of lines).
+// generate_text - array for writing the generated poem. To be completed with the function.
+// rhymes is an array where for each line there is a pointer to the beginning of the range of rhyming lines with it. It is filled lazy.
+// num_generate_lines - the size of the generated text.
 int nonsenseGenerator(char*** lines, int num_of_lines, char*** generate_text, char**** rhymes, int num_generate_lines)
 {
     if (*generate_text != nullptr || num_generate_lines % VERCE_SIZE != 0) {
@@ -248,32 +248,32 @@ int nonsenseGenerator(char*** lines, int num_of_lines, char*** generate_text, ch
     for (int i = 0; i < num_of_lines; ++i) {
         is_used[i] = false;
     }
-    //Генерация текста. На каждой итерации получается одна строфа.
-    //По умолчанию порядок рифм как в Онегине.
+    // Generate text. At each iteration, one stanza is obtained.
+    // Default order rhyme as like in Onegin.
     for (int i = 0; i < num_generate_lines; i += VERCE_SIZE) {
         int line_1, line_2;
-        //Четверостишие с перекрёстной рифмой
+        // Quatrain with cross rhyme
         makeRhyme(lines, num_of_lines, rhymes, &line_1, &line_2, &is_used);
         (*generate_text)[i] = (*lines)[line_1];
         (*generate_text)[i + 2] = (*lines)[line_2];
         makeRhyme(lines, num_of_lines, rhymes, &line_1, &line_2, &is_used);
         (*generate_text)[i + 1] = (*lines)[line_1];
         (*generate_text)[i + 3] = (*lines)[line_2];
-        //Четверостишие со смежной рифмой
+        // Quatrain with adjacent rhyme
         makeRhyme(lines, num_of_lines, rhymes, &line_1, &line_2, &is_used);
         (*generate_text)[i + 4] = (*lines)[line_1];
         (*generate_text)[i + 5] = (*lines)[line_2];
         makeRhyme(lines, num_of_lines, rhymes, &line_1, &line_2, &is_used);
         (*generate_text)[i + 6] = (*lines)[line_1];
         (*generate_text)[i + 7] = (*lines)[line_2];
-        //Четверостишие с кольцевой рифмой
+        // Quatrain with ring rhyme
         makeRhyme(lines, num_of_lines, rhymes, &line_1, &line_2, &is_used);
         (*generate_text)[i + 8] = (*lines)[line_1];
         (*generate_text)[i + 11] = (*lines)[line_2];
         makeRhyme(lines, num_of_lines, rhymes, &line_1, &line_2, &is_used);
         (*generate_text)[i + 9] = (*lines)[line_1];
         (*generate_text)[i + 10] = (*lines)[line_2];
-        //Ещё 2 рифмованные строки.
+        // Another 2 rhymed lines.
         makeRhyme(lines, num_of_lines, rhymes, &line_1, &line_2, &is_used);
         (*generate_text)[i + 12] = (*lines)[line_1];
         (*generate_text)[i + 13] = (*lines)[line_2];
@@ -325,7 +325,7 @@ int main()
         for (int i = 0; generate_text[i] != nullptr; ++i) {
             write(fd, generate_text[i], strlen(generate_text[i]));
             write(fd, "\n", 1);
-            //Сделаем пустую строку в конце каждой строфы.
+            // Make an empty line at the end of each verce.
             if (i % VERCE_SIZE == VERCE_SIZE - 1) {
                 write(fd, "\n", 1);
             }
