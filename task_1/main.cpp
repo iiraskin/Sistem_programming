687bc1c3040179c468dcd8efeec70bc6118b9277
Ilya Raskin <iraskin@mail.ru>
2018-09-18 01:29:13 +0300

    task 1

--- task_1/main.cpp
+++ task_1/main.cpp
@@ -6,27 +6,27 @@
 
 using namespace std;
 
-const int INF_ROOTS = 8; //Бесконечное количество корней.
-const int COMPLEX_ROOTS = 3; //Корни комплексные.
+const int INF_ROOTS = 8; // Infinite number of roots.
+const int COMPLEX_ROOTS = 3; // Roots are complex.
+const double EPSILON = 0.000001;
 
-//Решает уравнение вида c = 0.
+// Solves an equation with c = 0.
 int SolveConstantEquation(double c)
 {
-
     printf("# It's a Constant Equation\n");
-    if (c == 0) { //Т.е. уравнение имеет вид 0=0.
+    if (c == 0) { //Those the equation has the form 0 = 0.
         return INF_ROOTS;
-    } else {     //Уравнение имеет вид с=0.
+    } else {     // The equation has the form c = 0.
         return 0;
     }
 }
 
-//Решает линейное уравнение b*x + c = 0, где b != 0. Результат заносит в x.
+// Solve the linear equation b * x + c = 0, where b != 0. The result fills into x.
 int SolveLinearEquation(double b, double c, double *x)
 {
     if (b == 0) {
         printf("# It's not a Linear Equation\n");
-        return SolveConstantEquation(c); //Уравнение вырождается в линейное.
+        return SolveConstantEquation(c); // The equation degenerates into linear.
     }
 
     printf("# It's a Linear Equation\n");
@@ -34,10 +34,10 @@ int SolveLinearEquation(double b, double c, double *x)
     return 1;
 }
 
-//Решает квадратное уравнение a*x*x + b*x + c = 0. Результат заносит в x1 и x2.
-// в случае с комплексными корнями заносит в x1 их действительную часть, а в x2 - мнимую,
-//при этом достаточно помнить один корень: второй - комплексно-сопряжённый.
-//Возвращает количество корней.
+// Solve the quadratic equation a * x * x + b * x + c = 0. The result fills into x1 and x2.
+// in the case of complex roots inserts their real part in x1, and in x2 - imaginary,
+// In this case, it is enough to remember one root: the second is complex conjugate.
+// Returns the number of roots.
 int SolveSquareEquation(double a, double b, double c, double *x1, double *x2)
 {
     assert(isfinite(a));
@@ -50,22 +50,22 @@ int SolveSquareEquation(double a, double b, double c, double *x1, double *x2)
 
     if (a == 0) {
         printf("# It's not a Square Equation\n");
-        return SolveLinearEquation(b, c, x1); //Уравнение вырождается в линейное.
+        return SolveLinearEquation(b, c, x1); // The equation degenerates into linear.
     }
 
     printf("# It's a Square Equation\n");
     long double discriminant = b * b - 4 * a * c;
     printf("# Discriminant = %lg\n", discriminant);
-    if (discriminant < 0) { //Комплексные корни.
+    if (discriminant - 0 < EPSILON) { // Complex roots.
         *x1 = -b / (2 * a);
         *x2 = sqrt(-discriminant) / (2 * a);
         return COMPLEX_ROOTS;
     }
     *x1 = (-b + sqrt(discriminant)) / (2 * a);
-    if (discriminant == 0) {               //Корни совпадают
+    if (discriminant == 0) {               // Roots are the same
         *x2 = *x1;
         return 1;
-    } else {  //discriminant > 0
+    } else {  // discriminant > 0
         *x2 = (-b - sqrt(discriminant)) / (2 * a);
         return 2;
     }
@@ -80,7 +80,7 @@ int PrintRes(int num_of_roots, double x1, double x2)
                 break;
         case 2: printf ("x1 = %lg, x2 = %lg\n", x1, x2);
                 break;
-        //В случае комплексных корней в x1 и x2 хранятся их действительная и мнимая части соответственно.
+        // In the case of complex roots, their real and imaginary parts, respectively, are stored in x1 and x2.
         case COMPLEX_ROOTS: printf ("x1 = %lg + %lgi, x2 = %lg - %lgi\n", x1, x2, x1, x2);
                             break;
         case INF_ROOTS: printf("Any number\n");
@@ -126,10 +126,10 @@ const double PRECISION = 0.00001;
 
 int Testing()
 {
-    vector< array<double, 3> > tests; //вектор тестов, состоящий из троек a,b,c.
+    vector< array<double, 3> > tests; // vector of tests, consisting of triples a, b, c..
     vector< pair<int, pair<double, double> > > answers;
     MakeTests(tests, answers);
-    pair<int, pair<double, double> > program_answer; //количество корней, x1, x2
+    pair<int, pair<double, double> > program_answer; // number of roots, x1, x2
     for (int i = 0; i < tests.size(); ++i) {
         printf("----------------\n");
         printf("Test %d: %lgx^2+%lgx+%lg=0\n", i + 1, tests[i][0], tests[i][1], tests[i][2]);
@@ -143,7 +143,7 @@ int Testing()
             if (answers[i].first == 1 && abs(answers[i].second.first - program_answer.second.first) > PRECISION) {
                 throw;
             }
-            //Если корней 2 и они разные, то не забыть учесть, что они могут идти в ответе не в том порядке.
+            // If there are 2 roots and they are different, then don't forget to take into account that they can go in the wrong order.
             if (answers[i].first == 2 && (abs(answers[i].second.first - program_answer.second.first) > PRECISION ||
                                           abs(answers[i].second.second - program_answer.second.second) > PRECISION) &&
                 (abs(answers[i].second.first - program_answer.second.second) > PRECISION ||
