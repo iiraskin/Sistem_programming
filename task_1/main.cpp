df9570210b5c8553182561ff6bed02a882a8a5ab
Ilya Raskin <iraskin@mail.ru>
2018-09-20 22:43:29 +0300

    Some corrections

--- task_1/main.cpp
+++ task_1/main.cpp
@@ -2,29 +2,79 @@
 #include <cmath>
 #include <assert.h>
 #include <vector>
-#include <array>
 
-using namespace std;
+using std::vector;
+
+const double PRECISION = 0.00001; //Precision of calculating
+const double EPSILON = 0.000001;  //Precision of comparison of doubles;
 
 const int INF_ROOTS = 8; // Infinite number of roots.
 const int COMPLEX_ROOTS = 3; // Roots are complex.
-const double EPSILON = 0.000001;
+
+struct Test
+{
+    double a;
+    double b;
+    double c;
+
+    Test(double _a, double _b, double _c)
+    {
+        a = _a;
+        b = _b;
+        c = _c;
+    }
+};
+
+struct Answer
+{
+    int num; //number of roots
+    double x1;
+    double x2;
+
+    Answer(double _num, double _x1, double _x2)
+    {
+        num = _num;
+        x1 = _x1;
+        x2 = _x2;
+    }
+};
 
 // Solves an equation with c = 0.
+int SolveConstantEquation(double c);
+// Solve the linear equation b * x + c = 0, where b != 0. The result fills into x.
+int SolveLinearEquation(double b, double c, double *x);
+// Solve the quadratic equation a * x * x + b * x + c = 0. The result fills into x1 and x2.
+// in the case of complex roots inserts their real part in x1, and in x2 - imaginary,
+// In this case, it is enough to remember one root: the second is complex conjugate.
+// Returns the number of roots.
+int SolveSquareEquation(double a, double b, double c, double *x1, double *x2);
+int PrintRes(int num_of_roots, double x1, double x2);
+int EquationSolver();
+void MakeTests(vector<Test> &tests, vector<Answer> &answers);
+int Testing(const vector<Test>& tests, const vector<Answer>& answers);
+
+int main()
+{
+    vector<Test> tests;
+    vector<Answer> answers;
+    MakeTests(tests, answers);
+    Testing(tests, answers);
+    EquationSolver();
+}
+
 int SolveConstantEquation(double c)
 {
     printf("# It's a Constant Equation\n");
-    if (c == 0) { //Those the equation has the form 0 = 0.
+    if (abs(c) < EPSILON) { //Those the equation has the form 0 = 0.
         return INF_ROOTS;
     } else {     // The equation has the form c = 0.
         return 0;
     }
 }
 
-// Solve the linear equation b * x + c = 0, where b != 0. The result fills into x.
 int SolveLinearEquation(double b, double c, double *x)
 {
-    if (b == 0) {
+    if (abs(b) < EPSILON) {
         printf("# It's not a Linear Equation\n");
         return SolveConstantEquation(c); // The equation degenerates into linear.
     }
@@ -34,15 +84,11 @@ int SolveLinearEquation(double b, double c, double *x)
     return 1;
 }
 
-// Solve the quadratic equation a * x * x + b * x + c = 0. The result fills into x1 and x2.
-// in the case of complex roots inserts their real part in x1, and in x2 - imaginary,
-// In this case, it is enough to remember one root: the second is complex conjugate.
-// Returns the number of roots.
 int SolveSquareEquation(double a, double b, double c, double *x1, double *x2)
 {
-    assert(isfinite(a));
-    assert(isfinite(b));
-    assert(isfinite(c));
+    assert(std::isfinite(a));
+    assert(std::isfinite(b));
+    assert(std::isfinite(c));
 
     assert(x1 != nullptr);
     assert(x2 != nullptr);
@@ -53,6 +99,13 @@ int SolveSquareEquation(double a, double b, double c, double *x1, double *x2)
         return SolveLinearEquation(b, c, x1); // The equation degenerates into linear.
     }
 
+    if (abs(c) < EPSILON) {
+        printf("# It's a Square Equation, where c = 0\n");
+        *x1 = 0;
+        SolveLinearEquation(a, b, x2);
+        return 2;
+    }
+
     printf("# It's a Square Equation\n");
     long double discriminant = b * b - 4 * a * c;
     printf("# Discriminant = %lg\n", discriminant);
@@ -61,12 +114,13 @@ int SolveSquareEquation(double a, double b, double c, double *x1, double *x2)
         *x2 = sqrt(-discriminant) / (2 * a);
         return COMPLEX_ROOTS;
     }
-    *x1 = (-b + sqrt(discriminant)) / (2 * a);
+    double sqrt_of_discriminant = sqrt(discriminant);
+    *x1 = (-b + sqrt_of_discriminant) / (2 * a);
     if (abs(discriminant) < EPSILON) {               // Roots are the same
         *x2 = *x1;
         return 1;
     } else {  // discriminant > 0
-        *x2 = (-b - sqrt(discriminant)) / (2 * a);
+        *x2 = (-b - sqrt_of_discriminant) / (2 * a);
         return 2;
     }
 }
@@ -95,77 +149,70 @@ int EquationSolver()
 {
     printf ("# This program solve square equation a*x*x + b*x + c = 0\n");
     printf ("# Enter a, b, c\n");
-    double a, b, c;
+    double a = 0, b = 0, c = 0;
     scanf("%lg %lg %lg",&a, &b, &c);
-    double x1, x2;
+    double x1 = 0, x2 = 0;
     int num_of_roots = SolveSquareEquation(a, b, c, &x1, &x2);
     return PrintRes(num_of_roots, x1, x2);
 }
 
-void MakeTests(vector< array<double, 3> > &tests, vector< pair<int, pair<double, double> > > &answers)
+void MakeTests(vector<Test> &tests, vector<Answer> &answers)
 {
-    tests.push_back({1, 3, 2});
-    answers.push_back(make_pair(2, make_pair(-2, -1)));
-    tests.push_back({1, 4, 4});
-    answers.push_back(make_pair(1, make_pair(-2, -2)));
-    tests.push_back({0, 1, 4});
-    answers.push_back(make_pair(1, make_pair(-4, -4)));
-    tests.push_back({0, 0, 3});
-    answers.push_back(make_pair(0, make_pair(0, 0)));
-    tests.push_back({0, 0, 0});
-    answers.push_back(make_pair(INF_ROOTS, make_pair(0, 0)));
-    tests.push_back({1, 3, -4});
-    answers.push_back(make_pair(2, make_pair(-4, 1)));
-    tests.push_back({-2, 4, -2});
-    answers.push_back(make_pair(1, make_pair(1, 1)));
-    tests.push_back({1, 6, 10});
-    answers.push_back(make_pair(COMPLEX_ROOTS, make_pair(-3, 1)));
+    tests.push_back(Test(1, 3, 2));
+    answers.push_back(Answer(2, -2, -1));
+    tests.push_back(Test(1, 4, 4));
+    answers.push_back(Answer(1, -2, -2));
+    tests.push_back(Test(0, 1, 4));
+    answers.push_back(Answer(1, -4, -4));
+    tests.push_back(Test(0, 0, 3));
+    answers.push_back(Answer(0, 0, 0));
+    tests.push_back(Test(0, 0, 0));
+    answers.push_back(Answer(INF_ROOTS, 0, 0));
+    tests.push_back(Test(1, 3, -4));
+    answers.push_back(Answer(2, -4, 1));
+    tests.push_back(Test(-2, 4, -2));
+    answers.push_back(Answer(1, 1, 1));
+    tests.push_back(Test(1, 6, 10));
+    answers.push_back(Answer(COMPLEX_ROOTS, -3, 1));
+    tests.push_back(Test(1, 4, 0));
+    answers.push_back(Answer(2, 0, -4));
 }
 
-const double PRECISION = 0.00001;
-
-int Testing()
+int Testing(const vector<Test>& tests, const vector<Answer>& answers)
 {
-    vector< array<double, 3> > tests; // vector of tests, consisting of triples a, b, c..
-    vector< pair<int, pair<double, double> > > answers;
-    MakeTests(tests, answers);
-    pair<int, pair<double, double> > program_answer; // number of roots, x1, x2
+    Answer program_answer(0, 0, 0);
     for (int i = 0; i < tests.size(); ++i) {
         printf("----------------\n");
-        printf("Test %d: %lgx^2+%lgx+%lg=0\n", i + 1, tests[i][0], tests[i][1], tests[i][2]);
-        program_answer.first = SolveSquareEquation(tests[i][0], tests[i][1], tests[i][2],
-                                                   &program_answer.second.first, &program_answer.second.second);
-        PrintRes(program_answer.first, program_answer.second.first, program_answer.second.second);
+        printf("Test %d: %lgx^2+%lgx+%lg=0\n", i + 1, tests[i].a, tests[i].b, tests[i].c);
+        program_answer.num = SolveSquareEquation(tests[i].a, tests[i].b, tests[i].c, &program_answer.x1, &program_answer.x2);
+        PrintRes(program_answer.num, program_answer.x1, program_answer.x2);
         try {
-            if (program_answer.first != answers[i].first) {
+            if (program_answer.num != answers[i].num) {
                 throw;
             }
-            if (answers[i].first == 1 && abs(answers[i].second.first - program_answer.second.first) > PRECISION) {
+            if (answers[i].num == 1 && abs(answers[i].x1 - program_answer.x1) > PRECISION) {
                 throw;
             }
             // If there are 2 roots and they are different, then don't forget to take into account that they can go in the wrong order.
-            if (answers[i].first == 2 && (abs(answers[i].second.first - program_answer.second.first) > PRECISION ||
-                                          abs(answers[i].second.second - program_answer.second.second) > PRECISION) &&
-                (abs(answers[i].second.first - program_answer.second.second) > PRECISION ||
-                 abs(answers[i].second.second - program_answer.second.first) > PRECISION)) {
+            if (answers[i].num == 2 && (abs(answers[i].x1 - program_answer.x1) > PRECISION ||
+                                        abs(answers[i].x2 - program_answer.x2) > PRECISION) &&
+                                       (abs(answers[i].x1 - program_answer.x2) > PRECISION ||
+                                        abs(answers[i].x2 - program_answer.x1) > PRECISION)) {
                 throw;
             }
-            if (answers[i].first == COMPLEX_ROOTS && answers[i] != program_answer) {
+            if (answers[i].num == COMPLEX_ROOTS && (answers[i].num != program_answer.num ||
+                                                    answers[i].x1 != program_answer.x1 ||
+                                                    answers[i].x2 != program_answer.x2)) {
                 throw;
             }
         }
         catch(...) {
+            printf("!!!\n");
             printf("FALSE!\nThe real answer is:\n");
-            PrintRes(answers[i].first, answers[i].second.first, answers[i].second.second);
+            PrintRes(answers[i].num, answers[i].x1, answers[i].x2);
             continue;
         }
         printf("TRUE!\n");
     }
     return 0;
-}
-
-int main()
-{
-    Testing();
-    EquationSolver();
 }
